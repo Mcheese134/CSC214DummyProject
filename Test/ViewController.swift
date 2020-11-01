50a5eba585c48a1d7d281908cec47984bc9c92e1
Besmelh Alshaalan <besmelh.s@gmail.com>
2020-11-01 18:20:47 +0300

    Storyboard & VC
    Added a title in the storyboard, and functionality in the VC. (writes the textfield text in the label when button is clicked)

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -11,14 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var textField: UITextField!
-    
     @IBOutlet weak var mainLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func onClick(_ sender: Any) {
+        mainLabel?.text = textField?.text
     }
     
 }
