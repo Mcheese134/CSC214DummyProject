f33ea0c1128597d0a93520e9f6b4e6c645d673fd
Felix <Mcheese134@gmail.com>
2020-11-01 09:57:26 -0500

    Added storyboard items

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var mainLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onClick(_ sender: Any) {
+    }
+    
 }
 
